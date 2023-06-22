0b25e8e29665831b15d0cc4ece8a64ec7e2f5c60
KatrixReloaded <kartikeyasureka@gmail.com>
2023-06-23 00:35:06 +0530

    Importing ERC20

---
+++ src/Imports.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+import "../lib/solmate/src/tokens/ERC20.sol";
+
+contract Token is ERC20("name", "symbol", 18) {
+    
+}
