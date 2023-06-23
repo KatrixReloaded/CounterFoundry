4a2394e79a92f88e404f42f59d117cd4f65d87df
KatrixReloaded <kartikeyasureka@gmail.com>
2023-06-23 23:50:17 +0530

    Authentication test

--- test/Auth.t.sol
+++ test/Auth.t.sol
@@ -9,4 +9,29 @@ contract AuthTest is Test {
     function setUp() public {
         wallet = new Wallet();
     }
+
+    function testSetOwner() public {
+        wallet.setOwner(address(1));
+        assertEq(wallet.owner(), address(1));
+    }
+
+    function testFailNotOwner() public {
+        vm.prank(address(1));
+        wallet.setOwner(address(1));
+    }
+
+    function testFailNotOwnerAgain() public {
+        //msg.sender = address(this)
+        wallet.setOwner(address(1));
+
+        //msg.sender = address(1)
+        vm.startPrank(address(1));
+        wallet.setOwner(address(1));
+        wallet.setOwner(address(1));
+        wallet.setOwner(address(1));
+        vm.stopPrank();
+
+        //msg.sender = address(this)
+        wallet.setOwner(address(1));
+    }
 }
