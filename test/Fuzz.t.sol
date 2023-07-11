54b456f2f49b2b1a27591035071584b30cb40fa3
KatrixReloaded <kartikeyasureka@gmail.com>
2023-07-11 23:40:47 +0530

    Fuzz testing

--- test/Fuzz.t.sol
+++ test/Fuzz.t.sol
@@ -10,7 +10,7 @@ contract FuzzTest is Test {
         b = new Bit();
     }
 
-    function testMostSignificantBit(uint256 x) private pure returns (uint256) {
+    function mostSignificantBit(uint256 x) private pure returns (uint256) {
         uint256 i = 0;
         while((x >>= 1) > 0) {
             i++;
@@ -28,6 +28,16 @@ contract FuzzTest is Test {
     }
 
     function testMostSignificantBitFuzz(uint256 x) public {
+        //assume - if false, the fuzzer will discard the current fuzz inputs and start a new fuzz run
+        //skip x = 0
+        //vm.assume(x>0);
 
+        //bound(input, min, max) - bound input between min and max
+        x = bound(x, 1, 10);
+        assertGe(x, 1);
+        assertLe(x, 10);
+
+        uint i = b.mostSignificantBit(x);
+        assertEq(i, mostSignificantBit(x));
     }
 }
